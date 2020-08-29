fc1473e5a0ee715c4ac1410e13640ee483b6b0d0
ABC da contrucao <digital.abcdaconstrucao@gmail.com>
2020-08-28 22:17:04 -0300

    clone final

---
+++ src/components/AdBanner/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const AdBanner: React.FC = () => {
+  return (
+    <Container className="ad-banner">
+      <span>Lironnick - </span>
+      será que ficou bom.
+    </Container>
+  );
+};
+
+export default AdBanner;
